a204d32b18a2cecdb212587d1b201958c2b3a41f
MaxMariani <wmaxmariani@gmail.com>
2025-03-29 12:19:52 +0100

    Added Game Board

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
+
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, columnIndex) {
+        setGameBoard((previousGameBoard) => {
+            const updatedBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][columnIndex] = activePlayerSymbol
+            return updatedBoard;
+    } );
+
+    onSelectSquare();
+}
+
+return (
+    <ol id="game-board">
+        {gameBoard.map((row, rowIndex) => (
+            <li key={rowIndex}>
+                <ol>
+                    {row.map((playerSymbol, columnIndex) => (
+                        <li key={columnIndex}>
+                            <button onClick={() => handleSelectSquare(rowIndex, columnIndex)}> {playerSymbol} </button>
+                        </li>))}
+                </ol>
+            </li>))}
+
+    </ol>
+);
+
+};
